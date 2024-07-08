ea2524592fb03188cd716ea7bde8c6a8a4b25971
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-07-08 18:19:01 -0500

    .

--- app.py
+++ app.py
@@ -58,38 +58,7 @@ def get_text(user_prompt):
     print("Modified Product Description:")
     print(modified_description)
 
-def generate_voice(VOICE_ID, TEXT_TO_SPEAK):
-    CHUNK_SIZE = 1024
-    XI_API_KEY = "sk_e191793517086a66962297f7442ac33233cd635d232ec008"
-    OUTPUT_PATH = "output.mp3"
-
-    tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/stream"
-
-    headers = {
-        "Accept": "application/json",
-        "xi-api-key": XI_API_KEY
-    }
-
-    data = {
-        "text": TEXT_TO_SPEAK,
-        "model_id": "eleven_multilingual_v2",
-        "voice_settings": {
-            "stability": 0.5,
-            "similarity_boost": 0.8,
-            "style": 0.0,
-            "use_speaker_boost": True
-        }
-    }
-
-    response = requests.post(tts_url, headers=headers, json=data, stream=True)
-
-    if response.ok:
-        with open(OUTPUT_PATH, "wb") as f:
-            for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-                f.write(chunk)
-        print("Audio stream saved successfully.")
-    else:
-        print(response.text)
+d
 
 # Commented out IPython magic to ensure Python compatibility.
 def setup_pipeline():
